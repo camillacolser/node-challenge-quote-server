b5705d46b42486ed81cf8ee69eaff10ade9d22da
camilla.colomboserri <camilla.colomboserri@wallapop.com>
2022-11-26 11:26:10 +0100

    Add quotes and random quote routes

--- server.js
+++ server.js
@@ -1,35 +1,30 @@
-const { response } = require("express");
-// server.js
-// This is where your node app starts
+//const { response } = require("express");
 
-//load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
 
-//load the quotes JSON
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
+
 app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
-//START OF YOUR CODE...
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
 
-//...END OF YOUR CODE
+app.get("/quotes/random", function (request, response) {
+  response.send(pickFromArray(quotes))
+})
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
-function pickFromArray(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+function pickFromArray(quotes) {
+  const randomNumber = Math.random()
+  const totalNumberOfQuotes = quotes.length
+  const randomIndex = Math.floor(randomNumber * totalNumberOfQuotes)
+  return quotes[randomIndex];
 }
 
-//Start our server so that it listens for HTTP requests!
 let port = 5000;
 
 app.listen( port, function () {
